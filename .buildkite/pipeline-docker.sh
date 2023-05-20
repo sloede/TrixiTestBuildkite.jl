33258265e90331f72f9993008a2f5f44d398e29b
Michael Schlottke-Lakemper <michael@sloede.com>
2023-05-20 12:08:23 +0200

    Fix vars

--- .buildkite/pipeline-docker.sh
+++ .buildkite/pipeline-docker.sh
@@ -2,15 +2,17 @@
 
 set -euxo pipefail
 
-# Show current directory
+# Show current directory and its contents
 pwd
+find .
 
 # Set paths
 WORKING_DIR=$PWD
 JULIA_INSTALL_DIR=$(dirname $PWD)
 JULIA_VERSION_FULL=1.9.0
 JULIA_VERSION_MAJORMINOR=1.9
-JULIA_PREFIX=$JULIA_PREFIX/julia-${JULIA_VERSION_FULL}
+# JULIA_PREFIX=${JULIA_INSTALL_DIR}/julia-${JULIA_VERSION_FULL}
+JULIA_PREFIX=$PWD/julia-${JULIA_VERSION_FULL}
 export PATH="$JULIA_PREFIX/bin/:$PATH"
 
 # Install wget
@@ -18,7 +20,7 @@ apt-get update
 apt-get install -y wget
 
 # Install Julia
-cd $JULIA_PREFIX
+cd $JULIA_INSTALL_DIR
 wget https://julialang-s3.julialang.org/bin/linux/x64/${JULIA_VERSION_MAJORMINOR}/julia-${JULIA_VERSION_FULL}-linux-x86_64.tar.gz
 tar zxvf julia-${JULIA_VERSION_FULL}-linux-x86_64.tar.gz
 cd $WORKING_DIR
